baccdb8f3cf9fffda834ca19739e1d991f9068e9
NGAPE AKAME BRICE STEPHAN <bricestephan1985@gmail.com>
2018-05-04 09:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return"Happy holidays,${name}!"
+  return`Happy holidays, ${name}!`
 }
 function happyHolidaysTo(holiday, name){
-  return"Happy ${holiday},${name}!"
+  return'Happy ${holiday}, ${name}!'
 }
 function holidayCountdown(holiday, days){
   return "It's ${days} days until ${holiday}!"
